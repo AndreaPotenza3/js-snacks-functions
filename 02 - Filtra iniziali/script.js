2e7de811002316dbcab4f8d393db22f9d9198d1d
AndreaPotenza <andreapoten3@gmail.com>
2024-10-22 10:29:33 +0200

    corretto

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -5,10 +5,24 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function returnByLetter(array, letter = '') {
 
+    const result = []
+
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+        
+        if(element[0].toLocaleLowerCase() === letter.toLocaleLowerCase()) {
+            
+            result.push(element)
+        } 
+    }
+
+    return result
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(returnByLetter(names, 'A'))
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
