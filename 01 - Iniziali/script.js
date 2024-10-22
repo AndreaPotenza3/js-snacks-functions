51344bb7d4b1160110361cbdde69810c1546ca8c
AndreaPotenza <andreapoten3@gmail.com>
2024-10-22 10:04:26 +0200

    corretto

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -1,28 +1,32 @@
 /* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
-const indexNames =[]
+
 
 
 // Dichiara la funzione qui.
 
 //console.log(indexLetters(names))
 
-const indexChars = names.map(index => index.charAt(0));
-console.log(indexChars)
+//const indexChars = names.map(index => index.charAt(0));
+//console.log(indexChars)
 
 // Invoca la funzione qui e stampa il risultato in console
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
 function indexArrays(array) {
+
+const result = []
+
 for (let i = 0; i < array.length; i++) {
     const element = array[i];
-    const indexC = element.charAt(0)
+    const indexC = element[0]
     
-    return array
-
+    result.push(indexC)
    
 }
+
+return result
+
 }
-const indexnames = indexArrays(names)
-console.log(indexArrays(names))
+console.log(indexArrays(names)) 
